30868765e2c8a4873781e0fc81ae6af00a3aad88
ytvnr <yann.tavernier@graviteesource.com>
2024-09-05 15:35:22 +0200

    feat: implement mTLS policy https://gravitee.atlassian.net/browse/APIM-3985

--- src/test/java/io/gravitee/policy/mtls/MtlsPolicyIntegrationTest.java
+++ src/test/java/io/gravitee/policy/mtls/MtlsPolicyIntegrationTest.java
@@ -24,57 +24,153 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.gravitee.apim.gateway.tests.sdk.AbstractPolicyTest;
 import io.gravitee.apim.gateway.tests.sdk.annotations.DeployApi;
 import io.gravitee.apim.gateway.tests.sdk.annotations.GatewayTest;
+import io.gravitee.apim.gateway.tests.sdk.configuration.GatewayConfigurationBuilder;
 import io.gravitee.apim.gateway.tests.sdk.connector.EndpointBuilder;
 import io.gravitee.apim.gateway.tests.sdk.connector.EntrypointBuilder;
 import io.gravitee.common.http.HttpStatusCode;
+import io.gravitee.node.api.certificate.KeyStoreLoader;
 import io.gravitee.plugin.endpoint.EndpointConnectorPlugin;
 import io.gravitee.plugin.endpoint.http.proxy.HttpProxyEndpointConnectorFactory;
 import io.gravitee.plugin.entrypoint.EntrypointConnectorPlugin;
 import io.gravitee.plugin.entrypoint.http.proxy.HttpProxyEntrypointConnectorFactory;
 import io.gravitee.policy.mtls.configuration.MtlsPolicyConfiguration;
+import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.net.PemKeyCertOptions;
+import io.vertx.rxjava3.core.Vertx;
 import io.vertx.rxjava3.core.buffer.Buffer;
 import io.vertx.rxjava3.core.http.HttpClient;
 import io.vertx.rxjava3.core.http.HttpClientRequest;
+import java.net.URL;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 /**
  * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
  * @author GraviteeSource Team
  */
-@GatewayTest
-@DeployApi({ "/apis/v4/api.json" })
-public class MtlsPolicyIntegrationTest extends AbstractPolicyTest<MtlsPolicy, MtlsPolicyConfiguration> {
+public class MtlsPolicyIntegrationTest {
 
-    @Override
-    public void configureEntrypoints(Map<String, EntrypointConnectorPlugin<?, ?>> entrypoints) {
-        entrypoints.putIfAbsent("http-proxy", EntrypointBuilder.build("http-proxy", HttpProxyEntrypointConnectorFactory.class));
+    @Nested
+    @GatewayTest
+    @DeployApi({ "/apis/v4/api.json" })
+    class SecuredGateway extends AbstractPolicyTest<MtlsPolicy, MtlsPolicyConfiguration> {
+
+        @Override
+        public void configureEntrypoints(Map<String, EntrypointConnectorPlugin<?, ?>> entrypoints) {
+            entrypoints.putIfAbsent("http-proxy", EntrypointBuilder.build("http-proxy", HttpProxyEntrypointConnectorFactory.class));
+        }
+
+        @Override
+        public void configureEndpoints(Map<String, EndpointConnectorPlugin<?, ?>> endpoints) {
+            endpoints.putIfAbsent("http-proxy", EndpointBuilder.build("http-proxy", HttpProxyEndpointConnectorFactory.class));
+        }
+
+        @SneakyThrows
+        @Override
+        protected void configureGateway(GatewayConfigurationBuilder config) {
+            config
+                .httpSecured(true)
+                .set("http.ssl.clientAuth", "request")
+                .set("http.ssl.keystore.type", KeyStoreLoader.CERTIFICATE_FORMAT_SELF_SIGNED)
+                .set("http.ssl.truststore.path", getUrl("ca.pem").getPath())
+                .set("http.ssl.truststore.type", "pem")
+                .set("http.ssl.truststore.password", "secret");
+        }
+
+        @Test
+        protected void should_be_unauthorized_if_no_certificate_on_request(Vertx vertx) {
+            wiremock.stubFor(get("/endpoint").willReturn(ok("backend response")));
+
+            createTrustedHttpClient(vertx, false)
+                .rxRequest(HttpMethod.GET, "/test")
+                .flatMap(HttpClientRequest::rxSend)
+                .flatMap(response -> {
+                    assertThat(response.statusCode()).isEqualTo(HttpStatusCode.UNAUTHORIZED_401);
+                    return response.body();
+                })
+                .test()
+                .awaitDone(10, TimeUnit.SECONDS)
+                .assertComplete()
+                .assertValue(Buffer.buffer(MtlsPolicy.FAILURE_MESSAGE))
+                .assertNoErrors();
+
+            wiremock.verify(0, getRequestedFor(urlPathEqualTo("/endpoint")));
+        }
+
+        @Test
+        protected void should_call_the_api_with_a_certificate_on_request(Vertx vertx) {
+            wiremock.stubFor(get("/endpoint").willReturn(ok("backend response")));
+
+            createTrustedHttpClient(vertx, true)
+                .rxRequest(HttpMethod.GET, "/test")
+                .flatMap(HttpClientRequest::rxSend)
+                .flatMap(response -> {
+                    assertThat(response.statusCode()).isEqualTo(HttpStatusCode.OK_200);
+                    return response.body();
+                })
+                .test()
+                .awaitDone(10, TimeUnit.SECONDS)
+                .assertComplete()
+                .assertValue(Buffer.buffer("backend response"))
+                .assertNoErrors();
+
+            wiremock.verify(1, getRequestedFor(urlPathEqualTo("/endpoint")));
+        }
+
+        HttpClient createTrustedHttpClient(Vertx vertx, boolean withCert) {
+            var options = new HttpClientOptions().setSsl(true).setTrustAll(true).setDefaultPort(gatewayPort()).setDefaultHost("localhost");
+            if (withCert) {
+                options =
+                    options.setPemKeyCertOptions(
+                        new PemKeyCertOptions().addCertPath(getUrl("client.cer").getPath()).addKeyPath(getUrl("client.key").getPath())
+                    );
+            }
+
+            return vertx.createHttpClient(options);
+        }
+
+        URL getUrl(String name) {
+            return getClass().getClassLoader().getResource(name);
+        }
     }
 
-    @Override
-    public void configureEndpoints(Map<String, EndpointConnectorPlugin<?, ?>> endpoints) {
-        endpoints.putIfAbsent("http-proxy", EndpointBuilder.build("http-proxy", HttpProxyEndpointConnectorFactory.class));
-    }
+    @Nested
+    @GatewayTest
+    @DeployApi({ "/apis/v4/api.json" })
+    class UnsecuredGateway extends AbstractPolicyTest<MtlsPolicy, MtlsPolicyConfiguration> {
 
-    @Test
-    protected void should_call_api_with_policy_on_request(HttpClient httpClient) {
-        wiremock.stubFor(get("/endpoint").willReturn(ok("backend response")));
+        @Override
+        public void configureEntrypoints(Map<String, EntrypointConnectorPlugin<?, ?>> entrypoints) {
+            entrypoints.putIfAbsent("http-proxy", EntrypointBuilder.build("http-proxy", HttpProxyEntrypointConnectorFactory.class));
+        }
 
-        httpClient
-            .rxRequest(HttpMethod.GET, "/test")
-            .flatMap(HttpClientRequest::rxSend)
-            .flatMap(response -> {
-                assertThat(response.statusCode()).isEqualTo(HttpStatusCode.OK_200);
-                return response.body();
-            })
-            .test()
-            .awaitDone(10, TimeUnit.SECONDS)
-            .assertComplete()
-            .assertValue(Buffer.buffer("backend response"))
-            .assertNoErrors();
+        @Override
+        public void configureEndpoints(Map<String, EndpointConnectorPlugin<?, ?>> endpoints) {
+            endpoints.putIfAbsent("http-proxy", EndpointBuilder.build("http-proxy", HttpProxyEndpointConnectorFactory.class));
+        }
 
-        wiremock.verify(1, getRequestedFor(urlPathEqualTo("/endpoint")));
+        @Test
+        protected void should_be_unauthorized_if_no_certificate_on_request(HttpClient client) {
+            wiremock.stubFor(get("/endpoint").willReturn(ok("backend response")));
+
+            client
+                .rxRequest(HttpMethod.GET, "/test")
+                .flatMap(HttpClientRequest::rxSend)
+                .flatMap(response -> {
+                    assertThat(response.statusCode()).isEqualTo(HttpStatusCode.UNAUTHORIZED_401);
+                    return response.body();
+                })
+                .test()
+                .awaitDone(10, TimeUnit.SECONDS)
+                .assertComplete()
+                .assertValue(Buffer.buffer(MtlsPolicy.FAILURE_MESSAGE))
+                .assertNoErrors();
+
+            wiremock.verify(0, getRequestedFor(urlPathEqualTo("/endpoint")));
+        }
     }
 }
