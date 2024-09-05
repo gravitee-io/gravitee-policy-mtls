1e72fb4bc59db702731c0c1cc89b93a55dcc96d1
ytvnr <yann.tavernier@graviteesource.com>
2024-09-05 15:35:22 +0200

    feat: implement mTLS policy https://gravitee.atlassian.net/browse/APIM-3985

--- src/main/java/io/gravitee/policy/mtls/MtlsPolicy.java
+++ src/main/java/io/gravitee/policy/mtls/MtlsPolicy.java
@@ -31,13 +31,21 @@ package io.gravitee.policy.mtls;
  * limitations under the License.
  */
 
+import io.gravitee.common.http.HttpStatusCode;
+import io.gravitee.gateway.reactive.api.ExecutionFailure;
 import io.gravitee.gateway.reactive.api.context.HttpExecutionContext;
 import io.gravitee.gateway.reactive.api.policy.SecurityPolicy;
 import io.gravitee.gateway.reactive.api.policy.SecurityToken;
 import io.gravitee.policy.mtls.configuration.MtlsPolicyConfiguration;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Maybe;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.DigestUtils;
 
 /**
  * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
@@ -46,6 +54,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MtlsPolicy implements SecurityPolicy {
 
+    public static final String CLIENT_CERTIFICATE_MISSING = "CLIENT_CERTIFICATE_MISSING";
+    public static final String CLIENT_CERTIFICATE_INVALID = "CLIENT_CERTIFICATE_INVALID";
+    public static final String FAILURE_MESSAGE = "Unauthorized";
+    public static final String SSL_SESSION_REQUIRED = "SSL_SESSION_REQUIRED";
     private final MtlsPolicyConfiguration configuration;
 
     public MtlsPolicy(MtlsPolicyConfiguration configuration) {
@@ -58,8 +70,30 @@ public class MtlsPolicy implements SecurityPolicy {
     }
 
     @Override
-    public Maybe<SecurityToken> extractSecurityToken(HttpExecutionContext httpExecutionContext) {
-        return Maybe.just(SecurityToken.none());
+    public Maybe<SecurityToken> extractSecurityToken(HttpExecutionContext ctx) {
+        final SSLSession sslSession = ctx.request().sslSession();
+        if (sslSession == null) {
+            return Maybe.empty();
+        }
+
+        final Certificate[] peerCertificates;
+        try {
+            peerCertificates = sslSession.getPeerCertificates();
+        } catch (SSLPeerUnverifiedException e) {
+            return Maybe.empty();
+        }
+
+        if (peerCertificates == null || peerCertificates.length == 0) {
+            return Maybe.empty();
+        }
+
+        final String clientCertificate;
+        try {
+            clientCertificate = DigestUtils.md5DigestAsHex(peerCertificates[0].getEncoded());
+        } catch (CertificateEncodingException e) {
+            return Maybe.empty();
+        }
+        return Maybe.just(SecurityToken.forClientCertificate(clientCertificate));
     }
 
     @Override
@@ -79,6 +113,23 @@ public class MtlsPolicy implements SecurityPolicy {
 
     @Override
     public Completable onRequest(HttpExecutionContext ctx) {
+        final SSLSession sslSession = ctx.request().sslSession();
+        if (sslSession == null) {
+            return interruptWith401(ctx, SSL_SESSION_REQUIRED);
+        }
+        final Certificate[] certificates;
+        try {
+            certificates = sslSession.getPeerCertificates();
+        } catch (SSLPeerUnverifiedException e) {
+            return interruptWith401(ctx, CLIENT_CERTIFICATE_INVALID);
+        }
+        if (certificates == null || certificates.length == 0) {
+            return interruptWith401(ctx, CLIENT_CERTIFICATE_MISSING);
+        }
         return Completable.complete();
+    }
+
+    private static Completable interruptWith401(HttpExecutionContext ctx, String errorKey) {
+        return ctx.interruptWith(new ExecutionFailure(HttpStatusCode.UNAUTHORIZED_401).key(errorKey).message(FAILURE_MESSAGE));
     }
 }
